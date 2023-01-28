a4eb8a7a84b5008be8ea37e1e9299647a317e5e6
Thomas Martinsen <tma@venzo.com>
2023-01-28 10:04:10 +0100

    added initial lib

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# csharp
+# utilities
---
+++ src/common/CheckForNull.cs
@@ -0,0 +1,18 @@
+﻿/// Different ways to check for null
+/// Source:
+/// https://www.thomasclaudiushuber.com/2020/03/12/c-different-ways-to-check-for-null/
+
+public class CheckForNull
+{
+    public void Method01(string parameter)
+    {
+        _ = parameter ?? throw new ArgumentNullException(nameof(parameter));
+    }
+
+    public void Method02(string parameter)
+    {
+        if (parameter is null) { }
+
+        if (parameter is not null) { }
+    }
+}
